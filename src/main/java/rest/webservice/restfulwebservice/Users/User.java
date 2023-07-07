0b99fdae1f987ae5cfb462fda3d665600607b4c9
Aisha Amin <aisha.aminn@gmail.com>
2023-07-07 19:08:50 +0200

    Configuring Spring Data JPA and H2 Console

--- src/main/java/rest/webservice/restfulwebservice/Users/User.java
+++ src/main/java/rest/webservice/restfulwebservice/Users/User.java
@@ -1,11 +1,11 @@
 package rest.webservice.restfulwebservice.Users;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Date;
 
@@ -13,7 +13,11 @@ import java.util.Date;
 @Setter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message="Name should have atleast 2 characters")
---
+++ src/main/java/rest/webservice/restfulwebservice/Users/UserJPAController.java
@@ -0,0 +1,58 @@
+package rest.webservice.restfulwebservice.Users;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class UserJPAController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/jpa/users")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/jpa/users/{id}")
+    public EntityModel<User> getUserById(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+        if(!user.isPresent())
+            throw new UserNotFoundException("id:" + id);
+
+        EntityModel<User> resource = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder linkTo =
+                linkTo(methodOn(this.getClass()).getAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+        return resource;
+    }
+
+    @PostMapping("/jpa/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUserById(@PathVariable int id){
+        userRepository.deleteById(id);
+    }
+}
