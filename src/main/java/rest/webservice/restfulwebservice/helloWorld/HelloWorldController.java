8352da4beb5b04c39e149091a534c124a673768e
Aisha Amin <aisha.aminn@gmail.com>
2023-07-07 15:29:17 +0200

    Content Negotiation -> Accept Language Header (en or de)

--- src/main/java/rest/webservice/restfulwebservice/helloWorld/HelloWorldController.java
+++ src/main/java/rest/webservice/restfulwebservice/helloWorld/HelloWorldController.java
@@ -1,9 +1,12 @@
 package rest.webservice.restfulwebservice.helloWorld;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
@@ -24,4 +27,9 @@ public class HelloWorldController {
         return new HelloWorld(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message",locale);
+    }
 }
